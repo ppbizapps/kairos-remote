464f58ca326c7ce3e9109e83d34e81b617ef745e
Brian Hawkins <brianhks1@gmail.com>
2019-05-16 10:27:19 -0600

    bumped version

--- build.groovy
+++ build.groovy
@@ -28,7 +28,7 @@ saw.setProperty(Tablesaw.PROP_MULTI_THREAD_OUTPUT, Tablesaw.PROP_VALUE_ON)
 
 programName = "kairos-remote"
 //Do not use '-' in version string, it breaks rpm uninstall.
-version = "1.1"
+version = "1.2.0"
 release = saw.getProperty("KAIROS_RELEASE_NUMBER", "1") //package release number
 summary = "KairosDB Remote"
 description = """\
